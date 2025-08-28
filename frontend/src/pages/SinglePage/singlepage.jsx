5cebd40d84a1b0cdb8fad329761228011860b48e
Bert154-beep <masabayan15@gmail.com>
2025-08-28 23:02:01 +0500

    New

--- frontend/src/pages/SinglePage/singlepage.jsx
+++ frontend/src/pages/SinglePage/singlepage.jsx
@@ -263,15 +263,15 @@ export default function SinglePage() {
         <div className="max-w-7xl mx-auto px-8 py-15">
           <div className="grid lg:grid-cols-2 gap-12 items-center">
             <div className="rounded-2xl flex items-center justify-center p-4  ">
-            <div className="relative w-full h-80 rounded-2xl overflow-hidden border-8 border-white bg-white shadow-lg">
+              <div className="relative w-full h-80 rounded-2xl overflow-hidden border-8 border-white bg-white shadow-lg">
 
-  <img
-    src={data?.images?.[0] ? `/${data.images[0].replace(/\\/g, '/')}` : '/dummy.jpg'}
-    alt={data?.name}
-    className="w-full h-full object-cover"
-    onError={(e) => { e.target.src = "/dummy.jpg"; }}
-  />
-</div>
+                <img
+                  src={data?.images?.[0] ? `/${data.images[0].replace(/\\/g, '/')}` : '/dummy.jpg'}
+                  alt={data?.name}
+                  className="w-full h-full object-contain"
+                  onError={(e) => { e.target.src = "/dummy.jpg"; }}
+                />
+              </div>
 
 
 
